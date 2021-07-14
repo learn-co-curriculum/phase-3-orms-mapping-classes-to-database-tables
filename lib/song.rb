fd85fd84e536ef3f2c3dce9c669dcb4996e32692
ihollander <ianholla@gmail.com>
2021-07-14 10:36:41 -0400

    Added solution

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,43 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
   def initialize(name, album)
+    @id = nil
     @name = name
     @album = album
+  end
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    # insert the song
+    DB[:conn].execute(sql, self.name, self.album)
+
+    # get the song ID from the database and save it to the Ruby instance
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    # return the Ruby instance
+    self
+  end
+
+  def self.create(name:, album:)
+    song = Song.new(name, album)
+    song.save
   end
 
 end
