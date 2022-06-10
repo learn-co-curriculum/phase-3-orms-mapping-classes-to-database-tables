423419729c07abad5711527718de3f0288d03363
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-06-10 15:34:44 -0400

    commit tree f29f2c268e441391ae700bebcc9e8a14cfb1cdfe

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,42 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album ,:id
 
-  def initialize(name:, album:)
+  def initialize(name:, album: , id: nil)
     @name = name
     @album = album
+    @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY , 
+        name TEXT ,
+        album TEXT
+      )
+      SQL
+      DB[:conn].execute(sql)
+  end 
+
+  def save
+   sql = <<-SQL
+      INSERT INTO songs (name , album)
+      VALUES (? , ?)
+    SQL
+
+
+    # insert the song
+    DB[:conn].execute(sql, self.name, self.album)
+
+     # get the song ID from the database and save it to the Ruby instance
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    self
+  end 
+
+  def self.create(name: ,album:)
+    song = Song.new(name: name , album: album)
+    song.save
+  end 
 end
